735ff515f447e30370b3f4d7073952bb60168494
azamathasan <49510450+azamathasan@users.noreply.github.com>
2021-10-27 23:58:50 +0500

    Update webpack_for_scss.sh

--- webpack_for_scss.sh
+++ webpack_for_scss.sh
@@ -44,35 +44,35 @@ echo "write to webpack.config.js"
 
 #sed -i "module.exports = {" webpack.config.js
 
-echo "const path = require('path');" >> webpack.config.js
-echo "const miniCss = require('mini-css-extract-plugin');" >> webpack.config.js
+echo "const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+module.exports = {
+    mode: 'production',
+    // entry: './src/index.js',
+    entry: './scss/style.scss',  // entry file; it can be index.js with scss-import
+    output: {
+       filename: 'bundle_for_webpack.js', //output file; must have for webpack
+    //    filename: 'style.css',
+       path: path.resolve(__dirname, './js/')  // path for output-file
+    },
+    module: {
+       rules: [{
+          test:/\.(s*)css$/,
+          use: [
+             miniCss.loader,
+             'css-loader',    //plugins for css and sass/scss
+             'sass-loader',
+          ]
+       }]
+    },
+    plugins: [
+       new miniCss({
+          filename: '../css/style.css',  // output css-file
+       }),
+    ]
+ };" >> webpack.config.js
+echo  "************************END webpack.config.js**************************"
 
-echo "module.exports = {" >> webpack.config.js
-echo "    mode: 'production'," >> webpack.config.js
-echo "    // entry: './src/index.js'," >> webpack.config.js
-echo "    entry: './scss/style.scss',  // entry file; it can be index.js with scss-import" >> webpack.config.js
-echo "    output: {" >> webpack.config.js
-echo "       filename: 'bundle_for_webpack.js', //output file; must have for webpack" >> webpack.config.js
-echo "    //    filename: 'style.css'," >> webpack.config.js
-echo "       path: path.resolve(__dirname, './scss/')  // path for output-file" >> webpack.config.js
-echo "    }," >> webpack.config.js
-echo "    module: {" >> webpack.config.js
-echo "       rules: [{" >> webpack.config.js
-echo "          test:/\.(s*)css$/," >> webpack.config.js
-echo "          use: [" >> webpack.config.js
-echo "             miniCss.loader," >> webpack.config.js
-echo "             'css-loader',    //plugins for css and sass/scss" >> webpack.config.js
-echo "             'sass-loader'," >> webpack.config.js
-echo "          ]" >> webpack.config.js
-echo "       }]" >> webpack.config.js
-echo "    }," >> webpack.config.js
-echo "    plugins: [" >> webpack.config.js
-echo "       new miniCss({" >> webpack.config.js
-echo "          filename: '../css/style.css',  // output css-file" >> webpack.config.js
-echo "       })," >> webpack.config.js
-echo "    ]" >> webpack.config.js
-echo " };" >> webpack.config.js
-echo "************************END webpack.config.js**************************"
 
 
 
@@ -81,4 +81,4 @@ echo "************************END webpack.config.js**************************"
 echo "************************npm run build***********************"
 npm run build
 echo "************************END npm run build***********************"
-# npm run start 
+# npm run start - 
